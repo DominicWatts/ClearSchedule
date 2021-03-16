984e57bed085db88be8716fe35ebc31c5836509f
Dominic Watts <dominic.watts@hotmail.co.uk>
2021-03-16 21:39:41 +0000

    base

---
+++ Console/Command/Clear.php
@@ -0,0 +1,163 @@
+<?php
+/**
+ * Copyright © 2020 All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Xigen\ClearSchedule\Console\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Psr\Log\LoggerInterface;
+use Magento\Framework\Stdlib\DateTime\DateTime;
+use Magento\Framework\App\State;
+use Magento\Framework\App\Area;
+use Magento\Framework\App\ResourceConnection;
+
+class Clear extends Command
+{
+
+    const DELETE_DAYS = 1;
+    const LIMIT = 1000000;
+    
+    /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @var \Magento\Framework\App\State
+     */
+    private $state;
+
+    /**
+     * @var \Magento\Framework\Stdlib\DateTime\DateTime
+     */
+    private $dateTime;
+
+    /**
+     * @var InputInterface
+     */
+    protected $input;
+
+    /**
+     * @var OutputInterface
+     */
+    protected $output;
+
+    /**
+     * @var array
+     */
+    private $report = [];
+
+    /**
+     * @param \Psr\Log\LoggerInterface $logger
+     * @param \Magento\Framework\App\State $state
+     * @param \Magento\Framework\Stdlib\DateTime\DateTime $dateTime
+     * @param \Magento\Framework\App\ResourceConnection $resource
+     */
+    public function __construct(
+        LoggerInterface $logger,
+        State $state,
+        DateTime $dateTime,
+        ResourceConnection $resource
+    ) {
+        $this->logger = $logger;
+        $this->state = $state;
+        $this->dateTime = $dateTime;
+        $this->connection = $resource->getConnection();
+        $this->resource = $resource;
+        parent::__construct();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(
+        InputInterface $input,
+        OutputInterface $output
+    ) {
+
+        $this->input = $input;
+        $this->output = $output;
+
+        $this->state->setAreaCode(Area::AREA_GLOBAL);
+
+        $this->output->writeln((string) __(
+            '[%1] Start',
+            $this->dateTime->gmtDate()
+        ));
+
+        $this->output->writeln((string) __(
+            '[%1] Cleaning entries',
+            $this->dateTime->gmtDate()
+        ));
+
+        $this->tableName = $this->getTableName();
+        $this->startTime = time();
+        $this->report = [];
+
+        $select = $this->connection
+            ->select()
+            ->from($this->tableName)
+            ->where('scheduled_at < DATE_SUB(NOW(), INTERVAL ? DAY)', self::DELETE_DAYS)
+            ->limit(self::LIMIT);
+
+        $this->doDeleteFromSelect($select);
+
+        $this->output->writeln((string) __(
+            '[%1] Result : Duration %2 - Count %3 ',
+            $this->dateTime->gmtDate(),
+            $this->report['duration'] ?? 'NaN',
+            $this->report['count'] ?? 'NaN'
+        ));
+
+        $this->output->writeln((string) __(
+            '[%1] Finish',
+            $this->dateTime->gmtDate()
+        ));
+    }
+
+    /**
+     * Delete from select
+     * @param string $select
+     * @return array
+     */
+    public function doDeleteFromSelect($select)
+    {
+        try {
+            $this->report['duration'] = time() - $this->startTime;
+            // deliberate empty second argument
+            $query = $this->connection->deleteFromSelect($select, []);
+            $statement = $this->connection->query($query);
+            $this->report['count'] = $statement->rowCount();
+        } catch (\Exception $e) {
+            $this->logger->critical($e);
+        }
+        return $this->report;
+    }
+
+    /**
+     * Get cron schedule table name
+     * @return string
+     */
+    public function getTableName()
+    {
+        return $this->resource->getTableName('cron_schedule');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->setName("xigen:clearschedule:clear");
+        $this->setDescription("Clear cron schedule");
+        parent::configure();
+    }
+}
+
--- README.md
+++ README.md
@@ -1,53 +1,24 @@
-# Mage2 Module Xigen ClearSchedule
+# Xigen Magento 2 Clear Schedule
 
-    ``xigen/module-clearschedule``
+![phpcs](https://github.com/DominicWatts/ClearSchedule/workflows/phpcs/badge.svg)
 
- - [Main Functionalities](#markdown-header-main-functionalities)
- - [Installation](#markdown-header-installation)
- - [Configuration](#markdown-header-configuration)
- - [Specifications](#markdown-header-specifications)
- - [Attributes](#markdown-header-attributes)
+![PHPCompatibility](https://github.com/DominicWatts/ClearSchedule/workflows/PHPCompatibility/badge.svg)
+
+![PHPStan](https://github.com/DominicWatts/ClearSchedule/workflows/PHPStan/badge.svg)
 
 
-## Main Functionalities
-Magento 2 console command clear schedule
+Magento 2 console command clear cron schedule
 
-## Installation
-\* = in production please use the `--keep-generated` option
+# Install instructions
 
-### Type 1: Zip file
+`composer require dominicwatts/markerio`
 
- - Unzip the zip file in `app/code/Xigen`
- - Enable the module by running `php bin/magento module:enable Xigen_ClearSchedule`
- - Apply database updates by running `php bin/magento setup:upgrade`\*
- - Flush the cache by running `php bin/magento cache:flush`
+`php bin/magento setup:upgrade`
 
-### Type 2: Composer
+`php bin/magento setup:di:compile`
 
- - Make the module available in a composer repository for example:
-    - private repository `repo.magento.com`
-    - public repository `packagist.org`
-    - public github repository as vcs
- - Add the composer repository to the configuration by running `composer config repositories.repo.magento.com composer https://repo.magento.com/`
- - Install the module composer by running `composer require xigen/module-clearschedule`
- - enable the module by running `php bin/magento module:enable Xigen_ClearSchedule`
- - apply database updates by running `php bin/magento setup:upgrade`\*
- - Flush the cache by running `php bin/magento cache:flush`
+# Usage instructions
 
+   xigen:clearschedule:clear
 
-## Configuration
-
-
-
-
-## Specifications
-
- - Console Command
-	- Clear
-
-
-## Attributes
-
-
-
-# Magento 2 Clear Cron Schedule
+   php bin/magneto xigen:clearschedule:clear
